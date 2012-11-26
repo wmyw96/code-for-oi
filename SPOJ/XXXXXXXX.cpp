311ba4ddf43d0bf8ceb22f0d347dc1718fd3b594
Daniel Gu <wmyw96@126.com>
2012-11-26 20:12:58 +0800

    un_finished

--- SPOJ/XXXXXXXX.cpp
+++ SPOJ/XXXXXXXX.cpp
@@ -18,6 +18,8 @@ data T[MAX_N];
 int root[MAX_NLINE],pre[MAX_NN],a[MAX_NN];
 map<int,int> M;
 map<int,int> MM;
+set<int> ST[MAX_NN];
+set<int>::iterator tmp;
 int n,m,nm;
 
 data mp(int x,int y,int w){
@@ -74,16 +76,48 @@ int query(int x,int l,int r,int f,int t){
 	return ret;
 }
 
-void 
+void change(int i,int dat){
+	int pi=pre[i];
+	int ni=nxt[i];
+	
+    tmp=ST[MM[a[i]]].lower_bound(i);
+	ST[MM[a[i]]].erase(tmp);
+	tmp=ST[MM[dat]].lower_bound(i);
+	pre[i]=(tmp!=ST[MM[dat]].empty())?*tmp:0;
+	tmp=ST[MM[dat]].upper_bound(i);	
+	nxt[i]=(tmp!=ST[MM[dat]].empty())?*tmp:n+1;
+	
+	for (int l=1,r=n,x=1;;){
+		Delete(root[x],mp(a[i],pi,0));
+		Insert(root[x],mp(dat,pre[i],a[i]));
+		if (l==r) break;
+		int mid=(l+r)/2;
+		if (i<=mid) r=mid,x*=2; else l=mid+1,x=x*2+1;		
+	}
+	
+	nxt[pi]=ni;
+	int PI=ni;
+	for (int l=1,r=n,x=1;;){
+		Delete(root[x],mp(a[PI],pre[PI],0));
+		Insert(root[x],mp(a[PI],pi,a[PI]));
+		if (l==r) break;
+		int mid=(l+r)/2;
+		if (PI<=mid) r=mid,x*=2; else l=mid+1,x=x*2+1;
+	}
+	pre[PI]=pi;
+	
+}
 int main(){
 	scanf("%d",&n);
 	for (int i=1;i<=n;i++) scanf("%d",&a[i]);
 	for (int i=1;i<=n;i++){
 		pre[i]=M[a[i]];
+		nxt[M[a[i]]]=1;
 		M[a[i]]=i;
 		if (!MM[a[i]]) MM[a[i]]=++cnt;
 		ST[MM[a[i]]].insert(i);
 	}
+	for (int i=1;i<=n;i++) if (nxt[i]==0) nxt[i]=n+1;
 	Build(1,1,n);
 	scanf("%d",&m);
 	for (int i=1;i<=m;i++){
@@ -94,13 +128,6 @@ int main(){
 	}
 }
 /*
-5
-1 2 4 2 3                                                                              
-3
-Q 2 4
-Q 2 4
-*/
-
 5
 1 2 4 2 3                                                                              
 3
