1c3e6e65bf8faadbd53c944b55aa56af8c68f446
Daniel Gu <wmyw96@126.com>
2012-12-26 22:43:33 +0800

    solved

--- SPOJ/Accepted.cpp
+++ SPOJ/Accepted.cpp
@@ -1,2 +1,3 @@
-2012-12-11 12:02:54	Visible Lattice Points  accepted    VLATTICE    莫比乌斯反演 
-2012-12-11 06:11:30	Primes in GCD Table	    accepted	PGCD        莫比乌斯反演 
+2012-12-26 15:27:31	TABLE	   Mobius Inversion
+2012-12-11 12:02:54	VLATTICE   Mobius Inversion
+2012-12-11 06:11:30	PGCD         Mobius Inversion
