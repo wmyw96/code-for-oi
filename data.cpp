7f64ef778a5cfbf7dc582e21065aff63508d4d38
Wing Gu <wmyw96@126.com>
2013-02-25 16:11:33 +0800

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -12,6 +12,7 @@ int main(){
     srand(time(0));
     int n = 200;
     int m = 200;
+    printf("%d %d\n", n, m);
     for (int i = 1; i <= n; ++i){
         for (int j = 1; j <= m; ++j) printf("%c", rand() % 2 + 'a');
         printf("\n");
